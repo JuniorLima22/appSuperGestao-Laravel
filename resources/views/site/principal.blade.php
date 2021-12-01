354c7624a2390a57ef1b3a9367ac37bad0aa6aab
Junior Lima <juniorlima.dev@gmail.com>
2021-12-01 16:20:26 -0300

    Enviando o formulário de contato via POST

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -29,24 +29,25 @@
         <div class="contato">
             <h1>Contato</h1>
             <p>Caso tenha qualquer dúvida por favor entre em contato com nossa equipe pelo formulário abaixo.<p>
-            <form>
-                <input type="text" placeholder="Nome" class="borda-branca">
-                <br>
-                <input type="text" placeholder="Telefone" class="borda-branca">
-                <br>
-                <input type="text" placeholder="E-mail" class="borda-branca">
-                <br>
-                <select class="borda-branca">
-                    <option value="">Qual o motivo do contato?</option>
-                    <option value="">Dúvida</option>
-                    <option value="">Elogio</option>
-                    <option value="">Reclamação</option>
-                </select>
-                <br>
-                <textarea class="borda-branca">Preencha aqui a sua mensagem</textarea>
-                <br>
-                <button type="submit" class="borda-branca">ENVIAR</button>
-            </form>
+                <form action="{{ route('site.contato') }}" method="POST">
+                    @csrf
+                    <input type="text" name="nome" placeholder="Nome" class="borda-preta">
+                    <br>
+                    <input type="text" name="telefone" placeholder="Telefone" class="borda-preta">
+                    <br>
+                    <input type="text" name="email" placeholder="E-mail" class="borda-preta">
+                    <br>
+                    <select name="motivo_contato" class="borda-preta">
+                        <option value="">Qual o motivo do contato?</option>
+                        <option value="Dúvida">Dúvida</option>
+                        <option value="Elogio">Elogio</option>
+                        <option value="Reclamação">Reclamação</option>
+                    </select>
+                    <br>
+                    <textarea name="mensagem" class="borda-preta" placeholder="Preencha aqui a sua mensagem"></textarea>
+                    <br>
+                    <button type="submit" class="borda-preta">ENVIAR</button>
+                </form>
         </div>
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'TesteController@index')->name('site.principal');
 Route::get('/sobre-nos', 'TesteController@sobreNos')->name('site.sobre-nos');
 Route::get('/contato', 'TesteController@contato')->name('site.contato');
+Route::post('/contato', 'TesteController@contato')->name('site.contato');
