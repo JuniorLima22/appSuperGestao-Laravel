dc2a5d72d2ce90cef05460bc2205717742fbea54
Junior Lima <juniorlima.dev@gmail.com>
2021-12-07 14:03:35 -0300

    Adicionando softdelete no model Fornecedores

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -3,9 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Fornecedor extends Model
 {
+    // Trait
+    use SoftDeletes;
+    
     protected $table = 'fornecedores';
     protected $fillable = ['nome', 'site', 'uf', 'email'];
 }
