d95fca62d4e527489fdd787dcab5c53238adfad9
Junior Lima <juniorlima.dev@gmail.com>
2021-12-08 17:56:03 -0300

    Adicionando validação no formulário contato

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -14,6 +14,14 @@ class ContatoController extends Controller
 
     public function store(Request $request)
     {
+        $request->validate([
+            'nome' => 'required|min:3|max:40',
+            'telefone' => 'required',
+            'email' => 'required',
+            'motivo_contato' => 'required',
+            'mensagem' => 'required|max:200',
+        ]);
+        
         $contato = new SiteContato();
         $contato->fill($request->all());
         $contato->save();
